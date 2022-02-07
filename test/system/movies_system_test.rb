2039e155b3007ac948f97aa040cf9e70ff2b1595
Zachariah Cox <zachariahcox@github.com>
2022-02-07 15:02:47 -0500

    failing test

--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -2,6 +2,13 @@ require "application_system_test_case"
 
 class MoviesSystemTest < ApplicationSystemTestCase
   test "visiting the show" do
-    return false
+    visit("movies/1")
+    assert_text("Parasite")
+    assert_text "Bong Joon-ho"
+
+
+    # get "/movies/1"
+    # assert_response :success
+    # assert_selector "title", text:"Parasite"
   end
 end
