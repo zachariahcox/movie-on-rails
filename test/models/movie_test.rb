9567a42da7d36b4fc6fc591a65e939d8e1971c3d
Zachariah Cox <zachariahcox@github.com>
2022-02-08 14:41:45 -0500

    title test

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
     test "model is valid with a title" do
-        title = "foo"
-        m = Movie.new(title: "Parasite", director: "Bong Joon-ho")
+        title = "Parasite"
+        director = "Bong Joon-ho"
+        m = Movie.new(title: title, director: director)
+
         assert_equal m.title, title
+        assert_equal m.director, director
+    end
+
+    test "empty is not a valid title" do
+        refute Movie.new(title: "").valid?
+        refute Movie.new(title: nil).valid?
     end
 end
