eab9dee0a3151c0cc119fa2902e66c855cf802c5
Zachariah Cox <zachariahcox@github.com>
2022-02-08 12:59:42 -0500

    cleanup

--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -5,10 +5,5 @@ class MoviesSystemTest < ApplicationSystemTestCase
     visit movie_path(1)
     assert_text "Parasite"
     assert_text "Bong Joon-ho"
-
-
-    # get "/movies/1"
-    # assert_response :success
-    # assert_selector "title", text:"Parasite"
   end
 end
